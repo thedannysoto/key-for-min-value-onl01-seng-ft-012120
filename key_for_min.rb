87d0ea00a7226cc912c154494388ca55d88e802f
Danny Soto <87dsoto@gmail.com>
2020-01-23 19:58:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+
+# check for an empty hash
   if name_hash == {}
     return nil
   end 
+  
   min = Float::INFINITY
   min_key = ""
   name_hash.each do |key, val|
